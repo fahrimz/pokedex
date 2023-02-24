37821077838e034a5f858f1ceb0ba9ca2b2b34dd
Fahri <mzfahri620@gmail.com>
2023-02-24 15:01:07 +0700

    Initial Commit

---
+++ PokemonDemo/PokemonDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonDemoApp.swift
+//  PokemonDemo
+//
+//  Created by Fahri Zulkarnaen on 24/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
